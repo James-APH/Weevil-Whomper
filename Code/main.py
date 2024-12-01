2147cfa759803f47f28b768bede0147e68925e60
James Huston <jamzhuston@gmail.com>
2024-12-01 15:19:22 -0700

    added player and background to main, going to implement parallax first, then worry about animating the player

--- Code/main.py
+++ Code/main.py
@@ -1,13 +1,21 @@
 from settings import *
+from player import Player
+from background import *
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = self.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Autumn Explorer")
         self.clock = pygame.time.Clock()
         self.play = True
+
+        # Sprite Groups
+        self.main_sprites = pygame.sprite.Group()
+        self.background_sprites = pygame.sprite.Group()
+        self.player = Player((WIDTH / 2, HEIGHT / 2), self.main_sprites)
+        self.background = Background(self.background_sprites)
 
     def run(self):
         while self.play:
@@ -22,6 +30,9 @@ class Game:
             # update
 
             # draw
+            self.background.draw(self.screen)
+            self.main_sprites.update(dt)
+            self.main_sprites.draw(self.screen)
             pygame.display.update()
 
         pygame.quit()
