7bdb7e80cd2980334b672a44eec06585e41171e0
James Huston <jamzhuston@gmail.com>
2024-12-01 22:02:30 -0700

    added boundary code to the player class, now need to add animations

--- Code/player.py
+++ Code/player.py
@@ -14,23 +14,16 @@ class Player(pygame.sprite.Sprite):
     def get_input(self):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-        if int(keys[pygame.K_SPACE]):
-            pass  # implement jumping once player can move
+        # if int(keys[pygame.K_SPACE]):
+        #    pass  # implement jumping once player can move
 
     def move(self, dt):
-        if (
-            self.rect.left + (self.direction[0] * VELOCITY * dt) < 0 + self.width / 2
-        ) or (
-            self.rect.right + (self.direction[0] * VELOCITY * dt)
-            > WIDTH - self.width / 2
+        if (self.rect.left + (self.direction[0] * VELOCITY * dt) < BOUNDARY / 2) or (
+            self.rect.right + (self.direction[0] * VELOCITY * dt) > WIDTH - BOUNDARY / 2
         ):
             pass
         else:
             self.rect.center += self.direction * VELOCITY * dt
-
-    # $  and (self.rect.centerx + (self.direction[0] * VELOCITY * dt))
-    # ) < WIDTH - self.width:
-    #   self.rect.center += self.direction * VELOCITY * dt
 
     def update(self, dt):
         self.get_input()
