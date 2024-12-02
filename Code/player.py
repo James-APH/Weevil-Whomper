c71ff949063217a62d782b9b7aa49a57c614f077
James Huston <jamzhuston@gmail.com>
2024-12-01 20:47:00 -0700

    Updated player so that it is unable to go out of bounds

--- Code/player.py
+++ Code/player.py
@@ -9,7 +9,7 @@ class Player(pygame.sprite.Sprite):
         ).convert_alpha()
         self.rect = self.image.get_frect(center=coord)
         self.direction = pygame.math.Vector2(0, 0)
-        self.velocity = 300
+        self.width = self.image.get_width()
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -18,7 +18,19 @@ class Player(pygame.sprite.Sprite):
             pass  # implement jumping once player can move
 
     def move(self, dt):
-        self.rect.center += self.direction * VELOCITY * dt
+        if (
+            self.rect.left + (self.direction[0] * VELOCITY * dt) < 0 + self.width / 2
+        ) or (
+            self.rect.right + (self.direction[0] * VELOCITY * dt)
+            > WIDTH - self.width / 2
+        ):
+            pass
+        else:
+            self.rect.center += self.direction * VELOCITY * dt
+
+    # $  and (self.rect.centerx + (self.direction[0] * VELOCITY * dt))
+    # ) < WIDTH - self.width:
+    #   self.rect.center += self.direction * VELOCITY * dt
 
     def update(self, dt):
         self.get_input()
