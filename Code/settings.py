6d388bfdd94eb18684a94580ef5fb4ac12319d66
James Huston <jamzhuston@gmail.com>
2024-12-02 12:17:31 -0700

    created movement enum in settings to hold the different possible types of movement, sofar only left, right, and idle are implemented - but once I have those working others will be easier to add.

--- Code/settings.py
+++ Code/settings.py
@@ -1,5 +1,15 @@
 import pygame
 from os.path import join
+from enum import Enum
+
+
+class Movement(Enum):
+    LEFT = 1
+    RIGHT = 2
+    IDLE = 3
+    JUMP = 4
+    CROUCH = 5
+
 
 WIDTH = 1280
 HEIGHT = 640
